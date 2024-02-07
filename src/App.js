2e7240be9b606dc77774c16d1431fb7fd299bad2
kartik3yaS <kartikeyashukla009@gmail.com>
2024-02-07 14:25:52 +0530

    again commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Hobbies from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Skills />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,28 @@
+import Insta from '@iconscout/react-unicons/icons/uil-instagram'
+import Github from '@iconscout/react-unicons/icons/uil-github'
+import Linkedin from '@iconscout/react-unicons/icons/uil-linkedin'
+import footer from "../../Images/footer_img.png";
+import "./Footer.css";
+
+const Footer = () => {
+    return(
+        <div className="footer">
+            <img src={footer} alt="" style={{width: '100%', color: "#1E90FF"}} />
+            <div className="f-content">
+                <div className="f-icons">
+                    <a href="https://github.com/kartik3yaS" target="_blank">
+                        <Github color="white" size="3rem" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/kartikeya-shukla-63b433250/" target="_blank">
+                        <Insta color="white" size="3rem" />
+                    </a>
+                    <a href="https://www.instagram.com/kartikeya.22/?hl=en" target="_blank">
+                        <Linkedin color="white" size="3rem" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
