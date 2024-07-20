5dca60535902508934ded7d41ef7e8126d251906
kartik3yaS <kartikeyashukla009@gmail.com>
2024-07-21 01:57:20 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ import Hobbies from './components/About/About';
 import Skills from './components/Skills/Skills';
 import Portfolio from './components/Portfolio/Portfolio';
 import Contact from './components/Contact/Contact';
+import { Helmet } from 'react-helmet';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
+       <Helmet>
+        <title>My Portfolio</title>
+      </Helmet>
       <NavBar />
       <Intro />
       <Hobbies />
